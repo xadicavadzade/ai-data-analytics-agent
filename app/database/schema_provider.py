0bef4386b43539353feaebd71ac85b8a4ba794a9
xadicavadzade <xadicavadzade@gmail.com>
2026-07-23 18:24:02 +0400

    Add python-multipart

--- app/database/schema_provider.py
+++ app/database/schema_provider.py
@@ -1,17 +1,20 @@
 from app.database.connection import SQLiteConnection
+
+
 class SchemaProvider:
 
-    def __init__(self,connection : SQLiteConnection):
+    def __init__(self, connection: SQLiteConnection):
         self.connection = connection
-    async def get_schema(self,database_path:str) -> str:
+
+    async def get_schema(self, database_path: str) -> str:
         conn = await self.connection.connect(database_path)
-       
-        
-        try :
+
+        try:
             cursor = await conn.execute("""
-            SELECT name
-            FROM sqlite_master
-            WHERE type='table';
+                SELECT name
+                FROM sqlite_master
+                WHERE type = 'table'
+                AND name NOT LIKE 'sqlite_%';
             """)
 
             tables = await cursor.fetchall()
@@ -19,20 +22,25 @@ class SchemaProvider:
             schema = []
 
             for table in tables:
-                table_name = table[0] #table is tuple
+                table_name = table[0]
 
                 schema.append(f"Table: {table_name}")
 
-                cursor = await conn.execute(f"PRAGMA table_info({table_name})")
+                cursor = await conn.execute(
+                    f'PRAGMA table_info("{table_name}")'
+                )
+
                 columns = await cursor.fetchall()
-                
+
                 for column in columns:
-                    schema.append(f"- {column[1]} {column[2]}")
+                    column_name = column[1]
+                    column_type = column[2]
+
+                    schema.append(
+                        f"- {column_name}: {column_type}"
+                    )
+
+            return "\n".join(schema)
 
         finally:
             await conn.close()
-
-        return "\n".join(schema)
-
-
-
