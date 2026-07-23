8f4b19ef47188d026a1db9f2f600f8a02061ddcc
xadicavadzade <xadicavadzade@gmail.com>
2026-07-23 18:34:58 +0400

    Add python-multipart

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://willowy-tanuki-486155.netlify.app",
+        "https://ai-analytic-agent.netlify.app",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
     ],
