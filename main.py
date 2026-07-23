c24f5c70a0e9a6c557fdc8999b382934145fb8ca
xadicavadzade <xadicavadzade@gmail.com>
2026-07-23 13:54:57 +0400

    fix main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.analytics import router as analytics_router
 from app.api.health import router as health_router
 from app.api.clear import router as clear_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://willowy-tanuki-486155.netlify.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(analytics_router)
 app.include_router(health_router)
 app.include_router(clear_router)
-
